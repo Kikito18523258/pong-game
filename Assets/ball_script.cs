b2c55ed4fb9bb591264cc6fff3286e1b06f9b849
Kikito18523258 <18523258@students.uii.ac.id>
2020-03-23 10:58:38 +0700

    first commit

---
+++ Assets/ball_script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int speed=10;
+
+    void Start()
+    {
+        Debug.Log("Hello World!");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(2,2) * speed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
